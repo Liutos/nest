7d706d5fdf78ac1d2b54098d00a81b8cabbc490f
Liutos <mat.liutos@gmail.com>
2021-07-16 22:14:16 +0800

    chore: Add GitHub Actions configuration.

--- nest/repository/DDL/init_nest.sql
+++ nest/repository/DDL/init_nest.sql
@@ -6,7 +6,7 @@ CREATE TABLE `t_keyword` (
   `content` varchar(1000) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `ix__content` (`content`(100))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `t_location`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -16,7 +16,7 @@ CREATE TABLE `t_location` (
   `name` varchar(10) NOT NULL COMMENT '地点名',
   `user_id` bigint(20) NOT NULL COMMENT '创建者的ID',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `t_plan`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -26,9 +26,9 @@ CREATE TABLE `t_plan` (
   `duration` int(11) DEFAULT '0' COMMENT '持续时长',
   `location_id` bigint(20) DEFAULT NULL COMMENT '计划生效的地点的ID',
   `task_id` int(11) NOT NULL COMMENT '该计划所属的任务',
-  `location` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '' COMMENT '能够执行该任务的地点',
+  `location` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' COMMENT '能够执行该任务的地点',
   `repeat_interval` int(11) DEFAULT NULL COMMENT '配合“重复类型”，计算出下一次的触发时间',
-  `repeat_type` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '' COMMENT '表示这一次执行完任务后，下一次的触发时间如何计算',
+  `repeat_type` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' COMMENT '表示这一次执行完任务后，下一次的触发时间如何计算',
   `status` int(11) NOT NULL COMMENT '计划的状态',
   `trigger_time` datetime NOT NULL COMMENT '需要执行任务的时间',
   `visible_hours` json DEFAULT NULL,
@@ -62,17 +62,17 @@ CREATE TABLE `t_task_keyword` (
   `task_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `ix__keyword_id` (`keyword_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `t_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `t_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `email` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用于登录的邮箱',
-  `nickname` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '昵称',
-  `password_hash` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '原始密码和“盐”一起用哈希算法计算出的哈希值',
-  `salt` char(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '参与密码哈希计算的随机值',
+  `email` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用于登录的邮箱',
+  `nickname` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '昵称',
+  `password_hash` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '原始密码和“盐”一起用哈希算法计算出的哈希值',
+  `salt` char(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '参与密码哈希计算的随机值',
   `ctime` datetime NOT NULL,
   `mtime` datetime NOT NULL,
   PRIMARY KEY (`id`),

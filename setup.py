9236459097f5d92d0e5ee5c0efa185fbecfa2083
Liutos <mat.liutos@gmail.com>
2021-07-23 22:12:33 +0800

    chore: Install dependencies depends on `setup.py`.

---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf8 -*-
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nest",
+    version="0.0.1",
+    author="Liutos",
+    author_email="mat.liutos@gmail.com",
+    description="Nest of tasks and plans.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Liutos/nest",
+    project_urls={
+        "Bug Tracker": "https://github.com/Liutos/nest/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    include_package_data=True,
+    install_requires=[
+        "click",
+        "DBUtils",
+        "Flask",
+        "PyMySQL==1.0.2",
+        "pymysql-pooling",
+        "PyPika",
+        "redis",
+        "webargs",
+    ],
+)
